02a61e15db168797def198b91dbc6f48c42ff083
Andrew Tolvstad <CTGBandy@gmail.com>
2017-11-30 05:34:12 -0800

    Update rotary encoder with new semantics

--- src/peripheral/rotaryencoder.hpp
+++ src/peripheral/rotaryencoder.hpp
@@ -1,52 +1,34 @@
 #ifndef ROTARYENCODER_H
 #define ROTARYENCODER_H
 
+#include <stdint.h>
+
 namespace AVRSupport::Peripheral {
     struct RotaryEncoder {
-        enum struct Active : uint8_t { A, B, BOTH, NONE };
-        using Reader = bool (*)();
-
-    private:
-        struct State {
-            bool a, b;
+        struct QuaternaryStream {
+            uint8_t stream;
             
-            constexpr State() : a(false), b(false) {}
-            constexpr State(bool a, bool b) : a(a), b(b) {}
+            constexpr QuaternaryStream() : stream(0) {}
+            bool operator==(uint8_t const rhs) const { return stream == rhs; }
 
-            inline bool odd() { return a != b; }
-            inline bool even_on() { return a && b; }
-            inline bool even_off() { return !a && !b; }
+            void shift_in(uint8_t const state) { stream = stream<<2 | state; }
+            uint8_t previous() const { return stream & 0b11; }
         };
+        
+        QuaternaryStream stream;
 
-        State previous, current;
-        Reader read_a;
-        Reader read_b;
+        bool turned_cw()  const { return stream == 0b00'10'01'00; }
+        bool turned_ccw() const { return stream == 0b00'01'10'00; }
 
-    public:
-        constexpr RotaryEncoder(Reader a, Reader b) :
-            read_a(a), read_b(b) {}
-
-        inline void update() {
-            previous = current;
-            current = { read_a(), read_b() };
-        }
-
-        inline bool rising_edge() {
-            return previous.even_off() && current.odd();
-        }
-
-        inline bool falling_edge() {
-            return previous.even_on() && current.odd();
-        }
-
-        inline Active get_active() {
-            if (current.a) {
-                if (current.b) return Active::BOTH;
-                else           return Active::A;
-            } else {
-                if (current.b) return Active::B;
-                else           return Active::NONE;
+        void update(bool a, bool b) {
+            switch (stream.previous()) {
+                case 0b00: if (a == b) return;
+                case 0b01: if (b)      return;
+                case 0b10: if (a)      return;
+                default: return;
             }
+
+            stream.shift_in(a<<1|b);
         }
     };
 }
