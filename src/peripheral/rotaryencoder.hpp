d115c920d0793ba9d9c4171aafd5b8fd9d9a200d
Andrew Tolvstad <Andrew@localhost>
2017-12-10 14:12:47 -0800

    Fix rotary encoder edge case

--- src/peripheral/rotaryencoder.hpp
+++ src/peripheral/rotaryencoder.hpp
@@ -12,7 +12,9 @@ namespace AVRSupport::Peripheral {
             bool operator==(uint8_t const rhs) const { return stream == rhs; }
 
             void shift_in(uint8_t const state) { stream = stream<<2 | state; }
-            uint8_t previous() const { return stream & 0b11; }
+            void unshift() { stream >>= 2; }
+            uint8_t previous()    const { return (stream & 0b0011);    }
+            uint8_t penultimate() const { return (stream & 0b1100)>>2; }
             void clear() { stream = 0; }
         };
         
@@ -21,15 +23,28 @@ namespace AVRSupport::Peripheral {
         bool turned_right() const { return stream == 0b00'10'01'00; }
         bool turned_left()  const { return stream == 0b00'01'10'00; }
 
+        bool shift_mid_state(bool a, bool b, uint8_t join) {
+                if (b) return false; // Ignore
+                if (!a) return true; // Accept ground
+                if (stream.penultimate() == join) {
+                    // Reject backwards partial
+                    stream.unshift();
+                    return false;
+                }
+                return true; // Accept middle transition
+        }
+
         void update(bool a, bool b) {
+            uint8_t join = a<<1|b;
+
             switch (stream.previous()) {
-                case 0b00: if (a == b) return; else break;
-                case 0b01: if (b)      return; else break;
-                case 0b10: if (a)      return; else break;
+                case 0b00: if (a == b)                       return; break;
+                case 0b01: if (!shift_mid_state(a, b, join)) return; break;
+                case 0b10: if (!shift_mid_state(b, a, join)) return; break;
                 default: return;
             }
 
-            stream.shift_in(a<<1|b);
+            stream.shift_in(join);
         }
 
         void clear() { stream.clear(); }
--- test/rotaryencoder_test.cpp
+++ test/rotaryencoder_test.cpp
@@ -1,22 +1,119 @@
 #include <cassert>
+#include <array>
 
 #include "peripheral/rotaryencoder.hpp"
 
-using AVRSupport::Peripheral::RotaryEncoder;
+using Rotary = AVRSupport::Peripheral::RotaryEncoder;
 
-RotaryEncoder rotary;
+template<size_t count>
+using Actions = std::array<std::array<int, 2>, count>;
+
+template<size_t count>
+void rotary_apply(
+    Rotary &rotary,
+    Actions<count> &actions
+) {
+    for(auto &a : actions) rotary.update(a[0], a[1]);
+}
+
+template<typename Func>
+void times_do(int count, Func func) {
+    for(int i{0}; i<count; i++) func();
+}
+
+bool left_turn() {
+    Rotary rotary;
+    Actions<5> actions { 0,0, 0,1, 1,1, 1,0, 0,0 };
+
+    rotary_apply(rotary, actions);
+    return (
+        rotary.turned_left() &&
+        !rotary.turned_right()
+    );
+}
+
+bool right_turn() {
+    Rotary rotary;
+    Actions<5> actions { 0,0, 1,0, 1,1, 0,1, 0,0 };
+
+    rotary_apply(rotary, actions);
+    return (
+        rotary.turned_right() &&
+        !rotary.turned_left()
+    );
+}
+
+bool deduplicate() {
+    Rotary rotary;
+    times_do(10, [&](){ rotary.update(0,0); });
+    times_do(11, [&](){ rotary.update(0,1); });
+    times_do(9,  [&](){ rotary.update(1,1); });
+    times_do(14, [&](){ rotary.update(1,0); });
+    times_do(10, [&](){ rotary.update(0,0); });
+
+    return (
+        rotary.turned_left() &&
+        !rotary.turned_right()
+    );
+}
+
+bool no_false_positive() {
+    Rotary rotary;
+    Actions<5> actions = {
+         0, 0,
+         0, 1,
+         1, 1,
+         0, 1,
+         0, 0
+    };
+
+    rotary_apply(rotary, actions);
+    return (
+        !rotary.turned_left() &&
+        !rotary.turned_right()
+    );
+}
+
+bool no_false_negative_1() {
+    Rotary rotary;
+    Actions<9> actions {
+        0,0,
+        0,1,
+        1,1,
+        0,1,
+        1,1,
+        1,0,
+        1,1,
+        1,0,
+        0,0
+    };
+
+    rotary_apply(rotary, actions);
+    return(rotary.turned_left());
+}
+
+bool no_false_negative_2() {
+    Rotary rotary;
+    Actions<9> actions = {
+        0, 0,
+        0, 1,
+        1, 1,
+        1, 0,
+        1, 1,
+        0, 1,
+        1, 1,
+        1, 0,
+        0, 0
+    };
+
+    rotary_apply(rotary, actions);
+    return(rotary.turned_left());
+}
 
 int main() {
-    rotary.update(0,0);
-    rotary.update(0,1);
-    rotary.update(1,1);
-    rotary.update(0,1);
-    rotary.update(1,1);
-    rotary.update(1,0);
-    rotary.update(1,1);
-    rotary.update(1,0);
-    rotary.update(0,0);
-
-    assert(rotary.turned_left());
+    assert(deduplicate());
+    assert(no_false_positive());
+    assert(no_false_negative_1());
+    assert(no_false_negative_2());
 }
 
