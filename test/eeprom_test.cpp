e8fbe19348369b48dc2340c68de6bb122473c09a
Andrew Tolvstad <Andrew@localhost>
2018-02-20 15:49:53 -0800

    Improve EEPROM unit tests

--- test/eeprom_test.cpp
+++ test/eeprom_test.cpp
@@ -1,3 +1,5 @@
+#define private public
+
 #include <portlib/register.hpp>
 #include <portlib/eeprom.hpp>
 #include <utility/stddef.hpp>
@@ -17,15 +19,33 @@ struct MyStruct {
     uint8_t thingy;
 };
 
+using Eeprom = PortLib::Eeprom<EEPROM_SIZE>;
+using BufferEeprom = PortLib::BufferEeprom<EEPROM_SIZE>;
+template<typename Type>
+using ValueEeprom = PortLib::ValueEeprom<EEPROM_SIZE, Type>;
+
+template<typename Enum>
+void assert_enum(Enum e, uint8_t v) {
+    assert(static_cast<uint8_t>(e) == v);
+}
+
+void control_mask_test() {
+    assert_enum(Eeprom::ControlMask::read, 0b1);
+    assert_enum(Eeprom::ControlMask::write, 0b10);
+    assert_enum(Eeprom::ControlMask::master_write, 0b100);
+    assert_enum(Eeprom::ControlMask::interrupt, 0b1000);
+}
+
 void buffer_eeprom_test() {
-    PortLib::BufferEeprom<EEPROM_SIZE> eeprom{EEDR, EECR, EEAR};
+    BufferEeprom eeprom{EEDR, EECR, EEAR};
 }
 
 void value_eeprom_test() {
-    PortLib::ValueEeprom<EEPROM_SIZE, MyStruct> eeprom{EEDR, EECR, EEAR};
+    ValueEeprom<MyStruct> eeprom{EEDR, EECR, EEAR};
 }
 
 int main() {
+    control_mask_test();
     buffer_eeprom_test();
     value_eeprom_test();
     return 0;
