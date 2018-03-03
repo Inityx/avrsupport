1fbbd4410cc8eac603189f96e8066e7111fa3d95
Andrew Tolvstad <Andrew@localhost>
2018-03-02 18:54:38 -0800

    Extend native usart

--- src/serial/interface/uart.hpp
+++ src/serial/interface/uart.hpp
@@ -4,8 +4,15 @@
 namespace AvrSupport::Serial::Interface {
     template<typename SelfClass>
     struct Uart {
-       virtual uint8_t     read_byte()               = 0;
-       virtual SelfClass & write_byte(uint8_t const) = 0;
+        enum struct StopBits : uint8_t {
+            one = 1,
+            two = 2,
+        };
+
+        virtual SelfClass & set_stop_bits(StopBits const) = 0;
+
+        virtual uint8_t     read_byte()               = 0;
+        virtual SelfClass & write_byte(uint8_t const) = 0;
     };
 }
 
--- src/serial/native/usart.hpp
+++ src/serial/native/usart.hpp
@@ -11,7 +11,8 @@
 namespace AvrSupport::Serial::Native {
     struct Usart : public Interface::Uart<Usart> {
     private:
-        enum class ControlAMask : uint8_t {
+        // Physical control enums
+        enum struct ControlAMask : uint8_t {
             receive_complete  = 0b1000'0000,
             transmit_complete = 0b0100'0000,
             data_reg_empty    = 0b0010'0000,
@@ -21,17 +22,17 @@ namespace AvrSupport::Serial::Native {
             double_speed      = 0b0000'0010,
             multi_processor   = 0b0000'0001,
         };
-        enum class ControlBMask : uint8_t {
-            rx_complete_irq_enable = 0b1000'0000,
-            tx_complete_irq_enable = 0b0100'0000,
-            data_empty_irq_enable  = 0b0010'0000,
-            rx_enable              = 0b0001'0000,
-            tx_enable              = 0b0000'1000,
-            char_size_high         = 0b0000'0100,
-            ninth_rx_bit           = 0b0000'0010,
-            ninth_tx_bit           = 0b0000'0001,
+        enum struct ControlBMask : uint8_t {
+            rx_done_irq_on    = 0b1000'0000,
+            tx_done_irq_on    = 0b0100'0000,
+            data_empty_irq_on = 0b0010'0000,
+            rx_enable         = 0b0001'0000,
+            tx_enable         = 0b0000'1000,
+            char_size_high    = 0b0000'0100,
+            ninth_rx_bit      = 0b0000'0010,
+            ninth_tx_bit      = 0b0000'0001,
         };
-        enum class ControlCMask : uint8_t {
+        enum struct ControlCMask : uint8_t {
             usart_mode     = 0b11'00'0000,
             parity_mode    = 0b00'11'0000,
             stop_bits      = 0b00'00'1000,
@@ -41,20 +42,23 @@ namespace AvrSupport::Serial::Native {
             clock_polarity = 0b00'00'0001,
         };
 
-        static Utility::avr_size_t const CHAR_SIZE_LOW_OFFSET{1};
-        static uint8_t         const CHAR_SIZE_LOW_MASK {0b011};
-        static uint8_t         const CHAR_SIZE_HIGH_MASK{0b100};
-        enum class Mode : uint8_t {
+        enum struct Mode : uint8_t {
             async_usart = 0b00'00'0000,
             sync_usart  = 0b01'00'0000,
             master_spi  = 0b11'00'0000,
         };
-        enum class ParityMode : uint8_t {
+        enum struct ParityMode : uint8_t {
             disabled = 0b00'00'0000,
             even     = 0b00'10'0000,
             odd      = 0b00'11'0000,
         };
+        
+        // Static constants
+        static Utility::avr_size_t const CHAR_SIZE_LOW_OFFSET{1};
+        static uint8_t             const CHAR_SIZE_LOW_MASK {0b011};
+        static uint8_t             const CHAR_SIZE_HIGH_MASK{0b100};
 
+        // Registers
         PortLib::Register8
             data,
             control_a,
@@ -63,8 +67,26 @@ namespace AvrSupport::Serial::Native {
             baud_l,
             baud_h;
 
+        // Internal methods
+        template<typename MaskEnum> void set_config(
+            PortLib::Register8 & reg,
+            MaskEnum const mask,
+            bool const active
+        ) {
+            if (active) reg |=  static_cast<uint8_t>(mask);
+            else        reg &= ~static_cast<uint8_t>(mask);
+        }
+
+        template<typename MaskEnum> bool get_status(
+            PortLib::Register8 & reg,
+            MaskEnum const mask
+        ) {
+            return reg & static_cast<uint8_t>(mask);
+        }
+
     public:
-        enum class CharSize : uint8_t {
+        // Logical control enums
+        enum struct CharSize : uint8_t {
             five_bit  = 0b000,
             six_bit   = 0b001,
             seven_bit = 0b010,
@@ -72,17 +94,7 @@ namespace AvrSupport::Serial::Native {
             nine_bit  = 0b111,
         };
 
-        Usart & set_char_size(CharSize size) {
-            uint8_t const
-                high_bit( static_cast<uint8_t>(size) & CHAR_SIZE_HIGH_MASK),
-                low_bits((static_cast<uint8_t>(size) & CHAR_SIZE_LOW_MASK) << CHAR_SIZE_LOW_OFFSET);
-            control_b &= ~static_cast<uint8_t>(ControlBMask::char_size_high);
-            control_c &= ~static_cast<uint8_t>(ControlCMask::char_size_low);
-            control_b |= high_bit;
-            control_c |= low_bits;
-            return *this;
-        }
-
+        // Constructors
         constexpr Usart(
             PortLib::Register8 udrn,
             PortLib::Register8 ucsrna,
@@ -99,6 +111,30 @@ namespace AvrSupport::Serial::Native {
             baud_h   {ubrrnh}
         {}
 
+        // Configuration methods
+        Usart & set_char_size(CharSize const size) {
+            uint8_t const
+                size_num = static_cast<uint8_t>(size),
+                high_bit( size_num & CHAR_SIZE_HIGH_MASK),
+                low_bits((size_num & CHAR_SIZE_LOW_MASK) << CHAR_SIZE_LOW_OFFSET);
+
+            control_b &= ~static_cast<uint8_t>(ControlBMask::char_size_high);
+            control_c &= ~static_cast<uint8_t>(ControlCMask::char_size_low);
+            control_b |= high_bit;
+            control_c |= low_bits;
+            return *this;
+        }
+
+        Usart & set_stop_bits(StopBits const bits) {
+            bool setting;
+            switch (bits) {
+                case StopBits::one: setting = false; break;
+                case StopBits::two: setting = true;  break;
+            }
+            set_config(control_c, ControlCMask::stop_bits, setting);
+            return *this;
+        }
+
         Usart & set_baud_rate(uint32_t const rate, uint32_t const f_cpu) {
             uint16_t baud_setting = (f_cpu / 16) / rate;
             baud_l = static_cast<uint8_t>(baud_setting & 0xFF);
@@ -106,33 +142,25 @@ namespace AvrSupport::Serial::Native {
             return *this;
         }
 
-        template<typename MaskEnum>
-        void set_config(
-            PortLib::Register8 & reg,
-            MaskEnum const mask,
-            bool const active
-        ) {
-            if (active) reg |=  static_cast<uint8_t>(mask);
-            else        reg &= ~static_cast<uint8_t>(mask);
-        }
-
         Usart & set_tx_rx(bool const tx_active, bool const rx_active) {
             set_config(control_b, ControlBMask::tx_enable, tx_active);
             set_config(control_b, ControlBMask::rx_enable, rx_active);
             
-            // Required?
-            asm("nop");
-            asm("nop");
+            asm("nop"); asm("nop"); // Required?
 
             return *this;
         }
 
-        bool is_writing() {
-            return !(control_a & static_cast<uint8_t>(ControlAMask::data_reg_empty));
-        }
+        // Status methods
+        bool is_writable()      { return get_status(control_a, ControlAMask::data_reg_empty  ); }
+        bool is_data_received() { return get_status(control_a, ControlAMask::receive_complete); }
+        bool is_frame_error()   { return get_status(control_a, ControlAMask::frame_error     ); }
+        bool is_data_overrun()  { return get_status(control_a, ControlAMask::data_overrun    ); }
+        bool is_parity_error()  { return get_status(control_a, ControlAMask::parity_error    ); }
 
+        // Usage methods
         uint8_t read_byte() {
-            while (!(control_a & static_cast<uint8_t>(ControlAMask::receive_complete)));
+            while (!is_data_received());
             return data;
         }
 
@@ -142,7 +170,7 @@ namespace AvrSupport::Serial::Native {
         }
 
         Usart & sync_write_byte(uint8_t const value) {
-            while (is_writing());
+            while (!is_writable());
             return write_byte(value);
         }
     };
